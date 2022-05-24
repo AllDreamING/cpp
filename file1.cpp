c4696fddcb61e99528eae6658d90f2ccc889d308
AllDreamING <1031083335@qq.com>
2022-05-24 10:38:28 +0800

    const

---
+++ file1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+//int main()
+//{
+    //  const是用来定义常量的，常量定义之后无法修改值
+    //  #define只是做了简单的字符串替换
+    // const int a = 100;//必须进行初始化
+    const int a = 100;
+    int b = 200;
+    // cout<<a<<endl;
+    // cout<<b<<endl;
+//   return 0;
+//}
